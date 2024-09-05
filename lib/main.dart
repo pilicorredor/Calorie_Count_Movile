3f8dd502f658799419ac7af6e0a13705feeca07e
Andrea Sierra <andrea.sierra05@uptc.edu.co>
2024-09-04 19:18:17 -0500

    Add change views

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:calorie_counter/pages/home_page.dart';
+/*import 'package:calorie_counter/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -12,6 +12,32 @@ class MyApp extends StatelessWidget {
       title: 'Material App',
       initialRoute: 'home',
       routes: {'home': (_) => const HomePage()},
+    );
+  }
+}*/
+import 'package:calorie_counter/pages/home_page.dart';
+import 'package:calorie_counter/providers/ui_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => UIProvider()), // Proveedor para manejar el estado de la barra de navegación
+      ],
+      child: MaterialApp(
+        title: 'Calorie Counter',
+        initialRoute: 'home',
+        routes: {
+          'home': (_) => const HomePage(),
+        },
+      ),
     );
   }
 }
--- lib/widgets/home_page/custom_navigation_bar.dart
+++ lib/widgets/home_page/custom_navigation_bar.dart
@@ -1,11 +1,18 @@
+import 'package:calorie_counter/providers/ui_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CustomNavigationBar extends StatelessWidget {
   const CustomNavigationBar({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final uiProvider = Provider.of<UIProvider>(context);
+
     return BottomNavigationBar(
+      currentIndex: uiProvider.bnbIndex,
+      onTap: (int i) => uiProvider.bnbIndex = i,
+      
       type: BottomNavigationBarType.fixed, // Fuerza el modo fijo para mostrar los íconos correctamente
       items: const [
         BottomNavigationBarItem(label: 'Inicio', icon: Icon(Icons.home)),
